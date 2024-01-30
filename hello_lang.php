445fd4be1c051c9cbf3b0a0ce05ea3352cbc8ebc
macOS enjoyer <143117956+ioscreate@users.noreply.github.com>
2024-01-30 18:47:04 +0100

    Update hello_lang.php

--- hello_lang.php
+++ hello_lang.php
@@ -1,4 +1,9 @@
 <?php
-$output = shell_exec('cat index.php');
-echo "<pre>$output</pre>";
+$filename = 'index.php';
+
+if (file_exists($filename)) {
+    highlight_file($filename);
+} else {
+    echo 'Le fichier n\'existe pas.';
+}
 ?>

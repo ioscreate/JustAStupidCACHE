e3ca4b71cd1e86851a65b7ea83c5b9e07bab4b8a
macOS enjoyer <143117956+ioscreate@users.noreply.github.com>
2024-01-30 18:54:56 +0100

    Update hello_lang.php

--- hello_lang.php
+++ hello_lang.php
@@ -1,13 +1,5 @@
 <?php
-$remoteFile = 'https://raw.githubusercontent.com/ioscreate/JustAStupidCACHE/main/hello_lang.php';
-$localFile = 'index.php';
 
-// Télécharger le fichier localement
-file_put_contents($localFile, file_get_contents($remoteFile));
+echo '<?php echo $_SERVER["SERVER_NAME"]; ?>';
 
-if (file_exists($localFile)) {
-    include $localFile;
-} else {
-    echo 'Failed to download and include the file.';
-}
 ?>
